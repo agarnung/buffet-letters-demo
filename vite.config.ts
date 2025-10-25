29d8a5197ddd673bb37760d6dfffe846d722f153
Alejandro Garnung Menéndez <garnungalejandro@gmail.com>
2025-10-25 14:12:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/buffet-letters-demo/', 
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  }
+  base: '/buffet-letters-demo/',
 })
