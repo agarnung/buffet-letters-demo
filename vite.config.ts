cf4685bed4e8cc1f796bc09f4f96730b51f35ffa
Alejandro Garnung Menéndez <garnungalejandro@gmail.com>
2025-10-25 13:34:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './buffet-letters-demo',
   server: {
     port: 3000,
     host: true
